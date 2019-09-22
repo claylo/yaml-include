05dea9c000b950a49812b545edbc3565deb69385
Clay Loveless <clay@loveless.net>
2019-09-22 16:19:57 -0400

    chore(depdencies): updated dependencies

--- test/test-swagger.js
+++ test/test-swagger.js
@@ -4,12 +4,13 @@ import yamlinc from '..';
 import fs from 'fs';
 import p from 'path';
 
-var src, obj, expected;
+var src, obj, expected, objJson, expectedJson;
 
 test('enable modular Swagger 2.0 management', t => {
   yamlinc.setBaseFile(p.join(__dirname, 'fixtures', 'swagger', 'spec.yaml'));
   src = fs.readFileSync(yamlinc.basefile, 'utf8');
   obj = yaml.load(src, { schema: yamlinc.YAML_INCLUDE_SCHEMA, filename: yamlinc.basefile });
-  expected = require(p.join(__dirname, 'fixtures', 'swagger', 'petstore-swagger.json'));
+  expected = fs.readFileSync(p.join(__dirname, 'fixtures', 'swagger', 'petstore-swagger.json'), 'utf8');
+  expected = JSON.parse(expected);
   t.deepEqual(obj, expected);
 });
